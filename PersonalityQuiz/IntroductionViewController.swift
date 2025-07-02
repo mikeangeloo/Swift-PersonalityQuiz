380beeecc454de0467c0818b81641f80a9c4c19a
mikeangeloo <mikeangeloo87@outlook.com>
2025-07-01 19:13:47 -0500

    first commit

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Miguel Angel Ramirez Lopez on 01/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalityQuizTests/PersonalityQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityQuizTests.swift
+//  PersonalityQuizTests
+//
+//  Created by Miguel Angel Ramirez Lopez on 01/07/25.
+//
+
+import Testing
+@testable import PersonalityQuiz
+
+struct PersonalityQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
